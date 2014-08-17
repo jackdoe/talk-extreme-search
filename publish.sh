4fcab1fd0a6c07b3327ef006a1022d01b7c04523
borislav nikolov <jack@sofialondonmoskva.com>
2014-08-17 19:29:08 +0200

    add simple publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+if [ $# -eq 0 ]; then
+    echo "usage $0 'commit message'"
+    exit 1
+fi
+git checkout 20m
+git commit -a -m $1
+git push origin 20m
+git checkout gh-pages
+git merge 20m
+git push origin gh-pages
+git checkout 20m
